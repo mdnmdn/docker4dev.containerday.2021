3d096cc8cdce5f91c3bbae8c4c13bc2f7ac51e9d
Marco De Nittis <marco.denittis@gmail.com>
2021-10-07 00:15:34 +0200

    completed go sample

--- go-server/main.go
+++ go-server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"os"
 
@@ -23,6 +25,35 @@ func handleRequestRedisIncr(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(res, "go bare ", val)
 }
 
+func handleIndexTemplate(response http.ResponseWriter, request *http.Request) {
+	tmplt := template.New("index.html")
+	tmplt, err := tmplt.ParseFiles("static/index.html")
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	ctx := context.Background()
+
+	vars := struct {
+		Counter int
+	}{
+		int(rdb.Incr(ctx, "counter").Val()),
+	}
+	tmplt.Execute(response, vars)
+}
+
+func logging(logger *log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				logger.Println(r.Method, r.URL.Path, r.RemoteAddr)
+				//logger.Println(r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
+			}()
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	redisUrl := os.Getenv("REDIS_URL")
@@ -37,9 +68,15 @@ func main() {
 		})
 	}
 
-	fmt.Println("starting...")
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	logger.Println("Server is starting...")
 
-	http.HandleFunc("/", handleRequest)
-	http.HandleFunc("/redis/incr", handleRequestRedisIncr)
-	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	router := http.NewServeMux()
+	fs := http.FileServer(http.Dir("./static/assets"))
+	router.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	router.HandleFunc("/", handleIndexTemplate)
+
+	router.HandleFunc("/api", handleRequest)
+	router.HandleFunc("/api/redis/incr", handleRequestRedisIncr)
+	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), logging(logger)(router))
 }
