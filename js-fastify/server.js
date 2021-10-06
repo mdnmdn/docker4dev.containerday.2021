ee8d90d28670fd9bac42fd6d133dfdb48b041197
Marco De Nittis <marco.denittis@gmail.com>
2021-10-07 00:51:40 +0200

    node sample complete

--- js-fastify/server.js
+++ js-fastify/server.js
@@ -1,31 +1,52 @@
+const path = require('path');
 const fastifyFactory = require('fastify');
-const fastifyRedis = require('fastify-redis');
-const fastifyPostgres = require('fastify-postgres');
+const fastifyMongo = require('fastify-mongodb');
+const static = require('fastify-static');
+const pov = require('point-of-view');
+const ejs = require('ejs');
 
 const port = process.env.PORT || 3000;
-const redisConnectionString = process.env.REDIS_URL;
-const pgConnectionString = process.env.PG_URL;
+const mongoConnectionString = process.env.MONGO_URL;
 
 const fastify = fastifyFactory({
   logger: true,
 });
 
-if (redisConnectionString) {
-  fastify.register(fastifyRedis, {
-    url: redisConnectionString,
+fastify.register(pov, { engine: { ejs } } );
+
+fastify.register(static, {
+  root: path.join(__dirname, 'public/assets'),
+  prefix: '/assets/',
+})
+
+if (mongoConnectionString) {
+  fastify.register(fastifyMongo, {
+    url: mongoConnectionString,
+    forceClose: true,
   });
-  fastify.log.info(`Redis enabled on: ${redisConnectionString}`);
+  fastify.log.info(`Mongo enabled on: ${mongoConnectionString}`);
 }
 
-if (pgConnectionString) {
-  fastify.register(fastifyPostgres, {
-    url: pgConnectionString,
-  });
-  fastify.log.info(`PG enabled on: ${pgConnectionString}`);
+
+
+const increment = async (mongoDb) => {
+  const collection = fastify.mongo.db.collection('data');
+  
+  const res = await collection.update({ _id: 'counter' }, {
+    '$inc' : { 'value': 1 },
+  }, { upsert: true });
+  const counter = await collection.findOne({ _id: 'counter' });
+  return counter;
 }
 
-fastify.get('/', async (_req, _res) => {
-  return { 'msg': 'ok' };
+fastify.get('/', async (_req, res) => {
+  const counter = await increment();
+  res.view('/public/index.html', { counter: counter.value });
+})
+
+fastify.get('/api/counter', async (_req, _res) => {
+  const counter = await increment();
+  return { 'msg': 'ok', counter: counter.value };
 });
 
 fastify.listen(port,'0.0.0.0', (err) => {
